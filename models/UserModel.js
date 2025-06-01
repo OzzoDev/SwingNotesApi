ebe62edf5ab02a60ca057a59880c15d7f7103213
oscar <oscar.burman005@gmail.com>
2025-06-01 11:55:06 +0200

    Signup/login is working

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,21 +1,41 @@
 import { executeQuery } from "../utils/utils.js";
 
 const UserModel = {
-  create: async (name, email, passwordHash) => {
-    return (await executeQuery(`
-        INSERT INTO "user" (name, email, password_hash)
-        VALUES ($1, $2, $3)
-        RETURNING *;    
-    `),
-    [name, email, passwordHash])[0];
+  getByNameOrEmail: async (name, email) => {
+    return (
+      await executeQuery(
+        `
+        SELECT *
+        FROM "user"
+        WHERE name = $1 OR name = $2  
+    `,
+        [name, email]
+      )
+    )[0];
   },
   getByName: async (name) => {
-    return (await executeQuery(`
+    return (
+      await executeQuery(
+        `
         SELECT *
         FROM "user"
         WHERE name = $1    
-    `),
-    [name])[0];
+    `,
+        [name]
+      )
+    )[0];
+  },
+  create: async (name, email, passwordHash) => {
+    return (
+      await executeQuery(
+        `
+        INSERT INTO "user" (name, email, password_hash)
+        VALUES ($1, $2, $3)
+        RETURNING *;    
+    `,
+        [name, email, passwordHash]
+      )
+    )[0];
   },
 };
 
--- services/UserService.js
+++ services/UserService.js
@@ -1,28 +1,36 @@
+import { BadRequestError, ConflictError, NotFoundError } from "../errors/error.js";
 import UserModel from "../models/UserModel.js";
+import bcrypt from "bcryptjs";
 
 const UserService = {
   signup: async (name, email, password) => {
     const passwordHash = await bcrypt.hash(password, 10);
 
+    const isNotUniqueUser = await UserModel.getByNameOrEmail(name, email);
+
+    if (isNotUniqueUser) {
+      throw new ConflictError("Name or email already exists");
+    }
+
     const user = await UserModel.create(name, email, passwordHash);
 
     if (!user) {
-      return { err: 404 };
+      throw new NotFoundError("User not found");
     }
 
-    return { id: user.id, name: user.id, email: user.email, createdAt: user.created_at };
+    return { id: user.id, name: user.name, email: user.email, createdAt: user.created_at };
   },
   login: async (name, password) => {
-    const user = UserModel.getByName(name);
+    const user = await UserModel.getByName(name);
 
     if (!user) {
-      return { err: 404 };
+      throw new NotFoundError("User not found");
     }
 
     const isMatch = await bcrypt.compare(password, user.password_hash);
 
     if (!isMatch) {
-      return { err: 400 };
+      throw new BadRequestError("Invalid credentials");
     }
 
     return { id: user.id, name: user.id, email: user.email, createdAt: user.created_at };
