d29e0dcd88631149182b0022cedaafbcf707f693
oscar <oscar.burman005@gmail.com>
2025-06-01 10:16:50 +0200

    Added error middlewares

---
+++ middlewares/error.js
@@ -0,0 +1,37 @@
+import { ZodError } from "zod";
+import jwt from "jsonwebtoken";
+import { AppError, NotFoundError } from "../errors/error";
+
+export const errorHandler = (err, req, res, next) => {
+  console.error("Error caught by errorHandler:", err);
+
+  if (err instanceof ZodError) {
+    return res.status(400).json({
+      message: err.errors.map((e) => e.message).join(", "),
+      success: false,
+    });
+  }
+
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+      success: false,
+    });
+  }
+
+  if (err instanceof jwt.JsonWebTokenError) {
+    return res.status(401).json({
+      message: "Your session has expired, plase login in again to continue",
+      success: false,
+    });
+  }
+
+  res.status(500).json({
+    error: "Internal server error",
+    message: "An unknown error occurred",
+  });
+};
+
+export const notFoundHandler = (req, _, next) => {
+  next(new NotFoundError(`Could not find ${req.originalUrl}`));
+};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import "./config/db.js";
+import { errorHandler, notFoundHandler } from "./middlewares/error.js";
 
 dotenv.config();
 
@@ -14,5 +15,9 @@ app.use(cors());
 app.use(helmet());
 
 app.get("/", (req, res) => res.send("Swing note api"));
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
