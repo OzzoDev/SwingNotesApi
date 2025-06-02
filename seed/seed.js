3693c25b06303f470d9aba8cb908b003ee9ac0c2
oscar <oscar.burman005@gmail.com>
2025-06-02 17:13:16 +0200

    Fixed issue with seeding

--- seed/seed.js
+++ seed/seed.js
@@ -1,9 +1,16 @@
 import { initDB } from "../config/db.js";
+import { executeQuery } from "../utils/utils.js";
 import { seedNotes } from "./noteSeed.js";
 import { seedUsers } from "./userSeed.js";
 
 (async () => {
+  await executeQuery(`DROP TABLE IF EXISTS "user" CASCADE`);
+  await executeQuery(`DROP TABLE IF EXISTS "note" CASCADE`);
+  await executeQuery(`DROP SEQUENCE IF EXISTS note_id_seq CASCADE`);
+  await executeQuery(`DROP SEQUENCE IF EXISTS user_id_seq CASCADE`);
   await initDB();
   await seedUsers();
   await seedNotes();
+
+  process.exit();
 })();
